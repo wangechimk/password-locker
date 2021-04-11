6e17a196cba5314489dcc2dda62e9b3be72fd258
wangechimk <wangechik.michelle@gmail.com>
2021-04-11 22:53:48 +0300

    create credentials class

--- locker.py
+++ locker.py
@@ -1,3 +1,7 @@
+import string
+from random import choice
+
+
 class User :
     """
     create User class that generates new instances of a user.
@@ -19,3 +23,31 @@ class User :
             """
 
             User.user_list.append(self)
+
+            def get_user(self):
+        for user in User.user_list:
+            
+            if user.username == self.username and user.password == self.password:
+                return True
+        return False
+
+    def find_credentials(self):
+        credentials = [credential for credential in Credentials.credentials_list if credential.username == self.username and credential.password == self.password]
+        return credentials
+
+
+
+class Credentials:
+    Credentials_list =[]       
+
+    def __init__(self, credential, username, password=None):
+        self.username = username
+        self.password = password
+        self.credential = credential
+
+        def save_credential(self):
+        """
+        Method that saves a user's credentials to a credential's list
+        """
+
+       Credentials.credentials_list.append(self)
