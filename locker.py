29c6e5475d806b2f4eb2d0930e8780bf0ff31564
wangechimk <wangechik.michelle@gmail.com>
2021-04-11 17:41:33 +0300

    add user class that defines user instance

---
+++ locker.py
@@ -0,0 +1,14 @@
+class user :
+    """
+    create User class that generates new instances of a user.
+
+    """
+    user = []
+
+    def __init__(self,username,password):
+        """
+        method defining the user properties
+        """
+
+        self.username = username
+        self.password =password
